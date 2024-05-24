8e636070638fd0441c36f3d12b268d2ccf483abd
5L6C5B1 <147370986+5L6C5B1@users.noreply.github.com>
2024-05-25 00:58:11 +0800

    Add ErrorViewModel.cs

---
+++ OFOrder_LUCIA11507/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OFOrder_LUCIA11507.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
