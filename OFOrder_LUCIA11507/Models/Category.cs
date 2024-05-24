caba4344330f30f9cff31643ad7c596b245df241
5L6C5B1 <147370986+5L6C5B1@users.noreply.github.com>
2024-05-25 00:58:38 +0800

    Add Category.cs

---
+++ OFOrder_LUCIA11507/Models/Category.cs
@@ -0,0 +1,9 @@
+namespace OFOrder_LUCIA11507.Models
+{
+    public class Category
+    {
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+    }
+}
