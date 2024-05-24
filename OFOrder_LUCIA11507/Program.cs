d2e728c6d7d9eaa571df7913ef9ccab724cc39cd
5L6C5B1 <147370986+5L6C5B1@users.noreply.github.com>
2024-05-25 00:48:26 +0800

    Add Program.cs

---
+++ OFOrder_LUCIA11507/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using OFOrder_LUCIA11507.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "fooditems",
+    pattern: "/fooditems/{categorySlug?}",
+    defaults: new { controller = "FoodItems", action = "Shop" });
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<AppDbContext>();
+SeedData.SeedDatabase(context);
+
+app.Run();
