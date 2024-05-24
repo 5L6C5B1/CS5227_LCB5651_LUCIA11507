cf11aebaf2a8877a52ed653d9c9f705489fe936d
5L6C5B1 <147370986+5L6C5B1@users.noreply.github.com>
2024-05-25 02:04:06 +0800

    Add SmallCartViewModel.cs

---
+++ OFOrder_LUCIA11507/Models/ViewModels/SmallCartViewModel.cs
@@ -0,0 +1,8 @@
+namespace OFOrder_LUCIA11507.Models.ViewModels
+{
+    public class SmallCartViewModel
+    {
+        public int NumOfItems { get; set; }
+        public double TotalAmount { get; set; }
+    }
+}
