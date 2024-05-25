4d4586cfc2bf62a0a6546f871bd31a8a8c6be422
5L6C5B1 <147370986+5L6C5B1@users.noreply.github.com>
2024-05-25 12:53:38 +0800

    Add CartViewModel.cs

---
+++ OFOrder_LUCIA11507/Models/ViewModels/CartViewModel.cs
@@ -0,0 +1,8 @@
+namespace OFOrder_LUCIA11507.Models.ViewModels
+{
+    public class CartViewModel
+    {
+        public List<CartItem> CartItems { get; set; }
+        public double GrandTotal { get; set; }
+    }
+}
