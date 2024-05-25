567d952938ae6f70f76bde6b787d4c5cf1da6eb1
5L6C5B1 <147370986+5L6C5B1@users.noreply.github.com>
2024-05-26 01:30:17 +0800

    Update FileExtensionAttribute.cs
    Modified code for if statement.

--- OFOrder_LUCIA11507/Infrastructure/Validation/FileExtensionAttribute.cs
+++ OFOrder_LUCIA11507/Infrastructure/Validation/FileExtensionAttribute.cs
@@ -13,7 +13,7 @@ namespace OFOrder_LUCIA11507.Infrastructure.Validation
                 string[] extensions = { "jpg", "png" };
                 bool result = extensions.Any(x => extension.EndsWith(x));
 
-                if(result)
+                if(!result)
                 {
                     return new ValidationResult("Allowed extensions are .jpg and .png.");
                 }
