07faee3d17a88ef7d5d460512c4d9968fd2ff5f4
5L6C5B1 <147370986+5L6C5B1@users.noreply.github.com>
2024-05-26 00:56:08 +0800

    Update FoodItemsController.cs
    Updated pageSize to 20.

--- OFOrder_LUCIA11507/Areas/Admin/Controllers/FoodItemsController.cs
+++ OFOrder_LUCIA11507/Areas/Admin/Controllers/FoodItemsController.cs
@@ -15,7 +15,7 @@ namespace OFOrder_LUCIA11507.Areas.Admin.Controllers
         }
         public async Task<IActionResult> Shop(int p = 1)
         {
-            int pageSize = 6;
+            int pageSize = 20;
             ViewBag.PageNumber = p;
             ViewBag.PageRange = pageSize;
             ViewBag.TotalPages = (int)Math.Ceiling((decimal)_context.FoodItems.Count() / pageSize);
