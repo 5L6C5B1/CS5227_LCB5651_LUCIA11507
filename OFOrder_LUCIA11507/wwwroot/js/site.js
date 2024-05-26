012dd1922d70fd3b6e8218b59ade77ee67b0792d
5L6C5B1 <147370986+5L6C5B1@users.noreply.github.com>
2024-05-26 15:16:46 +0800

    Update site.js

--- OFOrder_LUCIA11507/wwwroot/js/site.js
+++ OFOrder_LUCIA11507/wwwroot/js/site.js
@@ -5,7 +5,7 @@
 $(function() {
     if ($("a.confirmDeletion").length) {
         $("a.confirmDeletion").click(() => {
-            if (!confirm("Confirm deletion"))
+            if (!confirm("Confirm deletion?"))
                 return false;
         });
     }
