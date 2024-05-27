2de0e283b42500af865c7a467498f5b843aca01f
5L6C5B1 <147370986+5L6C5B1@users.noreply.github.com>
2024-05-27 17:27:57 +0800

    Update FoodItemsController.cs

--- OFOrder_LUCIA11507/Controllers/FoodItemsController.cs
+++ OFOrder_LUCIA11507/Controllers/FoodItemsController.cs
@@ -1,35 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using OFOrder_LUCIA11507.Infrastructure;
 using OFOrder_LUCIA11507.Models;
 
-namespace OFOrder_LUCIA11507.Areas.Admin.Controllers
+namespace OFOrder_LUCIA11507.Controllers
 {
-    [Area("Admin")]
     public class FoodItemsController : Controller
     {
         private readonly AppDbContext _context;
-        private readonly IWebHostEnvironment _webHostEnvironment;
-        public FoodItemsController(AppDbContext context, IWebHostEnvironment webHostEnvironment)
+
+        public FoodItemsController(AppDbContext context)
         {
             _context = context;
-            _webHostEnvironment = webHostEnvironment;
         }
-        public async Task<IActionResult> Shop(int p = 1)
+
+        public async Task<IActionResult> Shop(string categorySlug = "", int p = 1)
         {
             int pageSize = 6;
             ViewBag.PageNumber = p;
             ViewBag.PageRange = pageSize;
-            ViewBag.TotalPages = (int)Math.Ceiling((decimal)_context.FoodItems.Count() / pageSize);
+            ViewBag.CategorySlug = categorySlug;
 
-            return View(await _context.FoodItems.OrderByDescending(p => p.ID).Include(p => p.Category).Skip((p - 1) * pageSize).Take(pageSize).ToListAsync());
-        }
+            if (categorySlug == "")
+            {
+                ViewBag.TotalPages = (int)Math.Ceiling((decimal)_context.FoodItems.Count() / pageSize);
 
-        public IActionResult Create()
-        {
-            ViewBag.Categories = new SelectList(_context.Categories, "ID", "Name");
-            return View();
+                return View(await _context.FoodItems.OrderByDescending(p => p.ID).Skip((p - 1) * pageSize).Take(pageSize).ToListAsync());
+            }
+
+            Category category = await _context.Categories.Where(c => c.Slug == categorySlug).FirstOrDefaultAsync();
+            if (category == null) return RedirectToAction("Shop");
+
+            var productsByCategory = _context.FoodItems.Where(p => p.CategoryID == category.ID);
+            ViewBag.TotalPages = (int)Math.Ceiling((decimal)productsByCategory.Count() / pageSize);
+
+            return View(await productsByCategory.OrderByDescending(p => p.ID).Skip((p - 1) * pageSize).Take(pageSize).ToListAsync());
         }
     }
 }
