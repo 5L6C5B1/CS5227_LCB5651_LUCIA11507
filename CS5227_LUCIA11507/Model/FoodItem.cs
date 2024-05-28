8b1ecdd0320433e730aa31ff9dd3d396932918bd
5651 <147370986+5L6C5B1@users.noreply.github.com>
2024-05-29 06:49:03 +0800

    Add Model folder

---
+++ CS5227_LUCIA11507/Model/FoodItem.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CS5227_LUCIA11507.Model
+{
+    public class FoodItem
+    {
+        public int ID { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [MaxLength(100)]
+        public string Category { get; set; }
+        [Precision(16, 2)]
+        public string Price { get; set; }
+        public string Description { get; set; }
+        [MaxLength(100)]
+        public string ImageFileName { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
