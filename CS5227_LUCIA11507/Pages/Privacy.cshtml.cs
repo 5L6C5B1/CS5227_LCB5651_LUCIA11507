60ee88458f6a4f3ee29f5eeff3e045bbc337ca59
5651 <147370986+5L6C5B1@users.noreply.github.com>
2024-05-29 06:38:16 +0800

    Add Pages

---
+++ CS5227_LUCIA11507/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CS5227_LUCIA11507.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
---
+++ CS5227_LUCIA11507/Pages/Services/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CS5227_LUCIA11507.Pages.Services
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+    }
+}
