fe6069c06a6a4d965e5d8b4b0a9db409c3e2aec8
5651 <147370986+5L6C5B1@users.noreply.github.com>
2024-05-30 05:26:34 +0800

    Update Program.cs
    Added code in Identity service to add roles

--- CS5227_LUCIA11507/Program.cs
+++ CS5227_LUCIA11507/Program.cs
@@ -9,7 +9,9 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
-builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
 
