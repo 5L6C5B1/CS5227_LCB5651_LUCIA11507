3908101359ab7d19fcba9c8e2aadbaa676f6cb23
5651 <147370986+5L6C5B1@users.noreply.github.com>
2024-05-29 11:33:07 +0800

    Add FoodItemDto.cs

---
+++ CS5227_LUCIA11507/Model/FoodItemDto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CS5227_LUCIA11507.Model
+{
+    public class FoodItemDto
+    {
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+        [Required, MaxLength(100)]
+        public string Category { get; set; }
+        [Required]
+        public string Price { get; set; }
+        public string? Description { get; set; }
+        [MaxLength(100)]
+        public IFormFile? ImageFile { get; set; }
+    }
+}
