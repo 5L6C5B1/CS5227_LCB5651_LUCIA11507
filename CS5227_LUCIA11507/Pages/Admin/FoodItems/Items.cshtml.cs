3c88f9f134463a36d52590dfc4b1f2faa1771739
5651 <147370986+5L6C5B1@users.noreply.github.com>
2024-05-29 11:29:56 +0800

    Add Admin folder

---
+++ CS5227_LUCIA11507/Pages/Admin/FoodItems/Items.cshtml.cs
@@ -0,0 +1,23 @@
+using CS5227_LUCIA11507.Model;
+using CS5227_LUCIA11507.Pages.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CS5227_LUCIA11507.Pages.Admin.FoodItems
+{
+    public class ItemsModel : PageModel
+    {
+        private readonly AppDbContext context;
+
+        public List<FoodItem> FoodItems { get; set; } = new List<FoodItem>();
+
+        public ItemsModel(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public void OnGet()
+        {
+            FoodItems = context.FoodItems.ToList();
+        }
+    }
+}
