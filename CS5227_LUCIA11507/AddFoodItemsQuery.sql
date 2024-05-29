8efb7991cac5da47997832e122e88a6d53ce8c76
5651 <147370986+5L6C5B1@users.noreply.github.com>
2024-05-29 20:10:55 +0800

    Add project files
    Added query to insert fooditems to database, and the scaffolding readme file that came with the identity

---
+++ CS5227_LUCIA11507/AddFoodItemsQuery.sql
@@ -0,0 +1,22 @@
+﻿INSERT INTO FoodItems (Name, Category, Price, Description, ImageFileName, CreatedAt)
+VALUES
+('Old World Steak', 'food', 31.00, '200 year-old steak fresh off the grill, very fresh despite its age.', 'brahminsteak.png', GETDATE()),
+('THE SANDWICH', 'food', 5.00, 'The classic sandwich eaten by every American.', 'sandwich.png', GETDATE()),
+('Planeta Caldereta', 'food', 14.90, 'Beef stew with vegetables and spices.', 'caldereta.png', GETDATE()),
+('Pepperoni Pizza', 'food', 10.80, 'Best enjoyed in the sewers with renaissance turtles.', 'pizza.png', GETDATE()),
+('8-bites', 'food', 8.00, 'A meal set with a beef burger, fries, and cola in low resolution.', 'lowres.jpg', GETDATE()),
+('Old World Noodles', 'food', 7.00, 'It could be ramen.', 'noodles.png', GETDATE()),
+
+('Sierra Madre Mocktail', 'drinks', 5.80, 'Tropical fruits blended and mixed with cloud residue.', 'mocktail.png', GETDATE()),
+('Chocolate Frappe', 'drinks', 4.80, 'Yummer. Thirst quenching chocolate with ice and milk. Quenched thirst.', 'frappe.png', GETDATE()),
+('Cyber Midnight', 'drinks', 2.50, 'This mocktail takes you to 2050, literally. Contains slight radiation.', 'dbhdrink.jpg', GETDATE()),
+('Nuclear Soda', 'drinks', 2.00, 'This poppy soda survived the nuclear blast of the Great War just for you.', 'soda.png', GETDATE()),
+('Biohazard', 'drinks', 0.01, 'It tastes better than it looks.', 'biohazard.jpg', GETDATE()),
+('Water', 'drinks', 1.00, 'Water.', 'water.png', GETDATE()),
+
+('Edens Sweetrolls', 'desserts', 7.00, 'Feeling stressed? Have a sweetroll', 'sweetrolls.png', GETDATE()),
+('Pollyanna', 'desserts', 6.50, 'A slice of butterscotch pie toppped with raspberry and whipped cream on top.', 'pollyanna.png', GETDATE()),
+('Pompeii', 'desserts', 50.00, 'A birthday cake sized chocolate lava cake.', 'pompeii.png', GETDATE()),
+('Old World Doughnuts', 'desserts', 1.50, 'A vanilla doughnut with a strawberry and blueberry glaze.', 'doughnut.png', GETDATE()),
+('Black Mesa', 'desserts', 19.40, 'A black forest cake made artificially from a research facility in Upper Michigan, UCA.', 'blackmesa.jpg', GETDATE()),
+('Ocean Waves', 'desserts', 5.00, 'Sea salt flavoured ice cream popsicles.', 'icecream.jpg', GETDATE());
