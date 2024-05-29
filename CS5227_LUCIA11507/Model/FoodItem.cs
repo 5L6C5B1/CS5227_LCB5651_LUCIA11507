d53c56702597d465bdbcab4335e88f64fc6821d0
5651 <147370986+5L6C5B1@users.noreply.github.com>
2024-05-29 16:06:05 +0800

    Update FoodItem.cs
    changed price data type

--- CS5227_LUCIA11507/Model/FoodItem.cs
+++ CS5227_LUCIA11507/Model/FoodItem.cs
@@ -11,7 +11,7 @@ namespace CS5227_LUCIA11507.Model
         [MaxLength(100)]
         public string Category { get; set; }
         [Precision(16, 2)]
-        public string Price { get; set; }
+        public decimal Price { get; set; }
         public string Description { get; set; }
         [MaxLength(100)]
         public string ImageFileName { get; set; }
