cb36347b9618075d8fb4e98e78469f23b6797b1a
5651 <147370986+5L6C5B1@users.noreply.github.com>
2024-05-29 12:09:48 +0800

    Update FoodItemDto.cs
    changed data type for price

--- CS5227_LUCIA11507/Model/FoodItemDto.cs
+++ CS5227_LUCIA11507/Model/FoodItemDto.cs
@@ -10,7 +10,7 @@ namespace CS5227_LUCIA11507.Model
         [Required, MaxLength(100)]
         public string Category { get; set; }
         [Required]
-        public string Price { get; set; }
+        public decimal Price { get; set; }
         public string? Description { get; set; }
         [MaxLength(100)]
         public IFormFile? ImageFile { get; set; }
