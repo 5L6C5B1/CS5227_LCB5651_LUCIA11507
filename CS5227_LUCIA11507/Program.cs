a80d94a057021a04ada742c26223458a987ce4c4
5651 <147370986+5L6C5B1@users.noreply.github.com>
2024-05-29 20:09:33 +0800

    Update Program.cs
    Added identity

--- CS5227_LUCIA11507/Program.cs
+++ CS5227_LUCIA11507/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
+builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<AppDbContext>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
