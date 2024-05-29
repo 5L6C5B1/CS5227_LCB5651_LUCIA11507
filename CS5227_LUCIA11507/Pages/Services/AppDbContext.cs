f0d1a5599e9bf741a303f8aa5cb2b09c274cd2d8
5651 <147370986+5L6C5B1@users.noreply.github.com>
2024-05-29 20:12:52 +0800

    Update AppDbContext.cs
    Added code for Identity Users

--- CS5227_LUCIA11507/Pages/Services/AppDbContext.cs
+++ CS5227_LUCIA11507/Pages/Services/AppDbContext.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.EntityFrameworkCore;
+using CS5227_LUCIA11507.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.General;
 
 namespace CS5227_LUCIA11507.Pages.Services
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<AppUser>
     {
-        public AppDbContext(DbContextOptions options) : base(options) { }
+        public AppDbContext(DbContextOptions options) : base(options) {
+        
+        }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var admin = new IdentityRole("admin");
+            admin.NormalizedName = "name";
+
+            var manager = new IdentityRole("manager");
+            admin.NormalizedName = "manager";
+
+            var client = new IdentityRole("client");
+            admin.NormalizedName = "client";
+
+            builder.Entity<IdentityRole>().HasData(admin, manager, client);
+        }
         public DbSet<FoodItem> FoodItems { get; set; }
-
     }
 }
