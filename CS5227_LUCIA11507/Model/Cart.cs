1cb045e15bd61ecccf0bcb6865e16765d7988e78
5651 <147370986+5L6C5B1@users.noreply.github.com>
2024-05-31 08:50:24 +0800

    Add Cart models

---
+++ CS5227_LUCIA11507/Model/Cart.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CS5227_LUCIA11507.Model
+{
+    public class Cart
+    {
+        public Cart(FoodItem fooditem)
+        {
+            ID = fooditem.ID;
+            Name = fooditem.Name;
+            Price = fooditem.Price;
+            Quantity = 1;
+        }
+
+        public Cart()
+        {
+
+        }
+
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        [Precision(16, 2)]
+        public decimal Price { get; set; }
+        public int FoodItemID { get; set; }
+        public FoodItem FoodItem { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ CS5227_LUCIA11507/Model/CartViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CS5227_LUCIA11507.Model
+{
+    public class CartViewModel
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal GrandTotal { get; set; }
+    }
+}
+
